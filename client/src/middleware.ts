92b4b8100264df70b4cbde7af07da3c63063ba8f
장재욱 <jju1221@jainwon.com>
2025-08-17 01:40:37 +0900

    클라이언트 미들웨어 수정

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -7,12 +7,12 @@ const protectedRoutes = ["/rooms"];
 const authRoutes = ["/login"];
 
 export function middleware(request: NextRequest) {
-  const refreshToken = request.cookies.get("refreshToken")?.value;
+  const accessToken = request.cookies.get("accessToken")?.value;
   const { pathname } = request.nextUrl;
 
   // 인증이 필요한 페이지에 접근하려고 할 때
   if (protectedRoutes.some((route) => pathname.startsWith(route))) {
-    if (!refreshToken) {
+    if (!accessToken) {
       // 로그인 페이지로 리다이렉트
       return NextResponse.redirect(new URL("/login", request.url));
     }
@@ -20,7 +20,7 @@ export function middleware(request: NextRequest) {
 
   // 로그인 페이지에 이미 로그인된 상태로 접근하려고 할 때
   if (authRoutes.includes(pathname)) {
-    if (refreshToken) {
+    if (accessToken) {
       // 채팅방 목록 페이지로 리다이렉트
       return NextResponse.redirect(new URL("/rooms", request.url));
     }
@@ -28,7 +28,7 @@ export function middleware(request: NextRequest) {
 
   // 루트 경로로 접근할 때
   if (pathname === "/") {
-    if (refreshToken) {
+    if (accessToken) {
       // 로그인된 상태면 채팅방 목록으로
       return NextResponse.redirect(new URL("/rooms", request.url));
     } else {
