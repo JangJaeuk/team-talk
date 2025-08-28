badb196952fb4aa220f9adec5dec20da6d9250d0
장재욱 <jju1221@jainwon.com>
2025-08-28 15:01:09 +0900

    로그아웃 버튼에 피드백 ui 추가

--- client/src/components/chat/room-list/layout/RoomListHeader.tsx
+++ client/src/components/chat/room-list/layout/RoomListHeader.tsx
@@ -1,12 +1,20 @@
+import { LoadingSpinner } from "@/components/common/LoadingSpinner";
 import { useAuthStore } from "@/store/useAuthStore";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const RoomListHeader = () => {
   const router = useRouter();
   const { user, logout } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleLogout = () => {
-    logout(() => router.push("/login"));
+  const handleLogout = async () => {
+    setIsLoading(true);
+    try {
+      await logout(() => router.push("/login"));
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -16,9 +24,13 @@ export const RoomListHeader = () => {
         <span className="mr-4">{user?.nickname}님 환영합니다!</span>
         <button
           onClick={handleLogout}
-          className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+          disabled={isLoading}
+          className={`bg-red-500 text-white px-3 py-1.5 rounded flex items-center justify-center space-x-2 w-[90px] whitespace-nowrap text-sm ${
+            isLoading ? "opacity-75 cursor-not-allowed" : "hover:bg-red-600"
+          }`}
         >
-          로그아웃
+          {isLoading && <LoadingSpinner />}
+          <span>로그아웃</span>
         </button>
       </div>
     </div>
