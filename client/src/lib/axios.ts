e1c35b2ad9922dddb734175af6f88dbed8aa3522
장재욱 <jju1221@jainwon.com>
2025-09-01 11:05:31 +0900

    인증 에러 로그 추가

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -51,10 +51,12 @@ class HttpClient {
     this.api.interceptors.response.use(
       (response) => response,
       async (error) => {
+        console.log("response interceptor error:", error);
         const originalRequest = error.config;
 
         // Access Token이 없거나 만료된 경우 refresh 시도
         if (error.response?.status === 401 && !originalRequest._retry) {
+          console.log("trying to refresh token...");
           if (this.refreshing) {
             // 토큰 갱신 중이면 대기
             return new Promise((resolve) => {
@@ -69,7 +71,9 @@ class HttpClient {
           this.refreshing = true;
 
           try {
+            console.log("refreshAccessToken start");
             const token = await this.refreshAccessToken();
+            console.log("refreshAccessToken result:", token);
             if (token) {
               // 대기 중인 요청들 처리
               this.onRefreshSuccess(token);
